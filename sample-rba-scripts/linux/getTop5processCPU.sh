761879a0b4a88580a73ba1cddcaad745d60089c7
davidVistara <David.Portnoy@vistarait.com>
2016-04-05 15:39:02 -0700

    GetTop5_CPU_Processes.sh
    GetTop5_CPU_Processes.sh

--- sample-rba-scripts/linux/getTop5processCPU.sh
+++ sample-rba-scripts/linux/getTop5processCPU.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# run ps command, sort by highest cpu, list top 5, and print CPU% and process command
+# run ps command, sort by highest cpu, list top 5, and print CPU% and process command.
 
 
 ps aux | sort -rk 3,3 | head -n 6  |  awk '{print $3 "\t" $11}'
